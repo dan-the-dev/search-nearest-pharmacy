bd3bd9250d010ef46942788810e2f8baf71bd0bb
Daniele Scillia <daniele.scillia@gmail.com>
2020-09-29 00:00:05 +0200

    Set up of composer install container to install dependencies

--- src/index.php
+++ src/index.php
@@ -1,3 +1,11 @@
 <?php
 
-phpinfo();
+require __DIR__ . '/vendor/autoload.php';
+
+use Datto\JsonRpc\Http\Examples\Evaluator;
+use Datto\JsonRpc\Http\Examples\AuthenticatedServer;
+
+$evaluator = new Evaluator();
+$server = new AuthenticatedServer($evaluator);
+
+$server->reply();
